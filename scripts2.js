be34c557d8a99f0e2d9eb9c4e5d11e9658bf0bf3
SebastianMurcia23 <100739389+SebastianMurcia23@users.noreply.github.com>
2024-03-15 10:18:41 -0500

    .

--- scripts2.js
+++ scripts2.js
@@ -3,6 +3,7 @@ function textImg(card) {
     var img = card.querySelector('.img1');
     var button = card.querySelector('.Button');
 
+
     if (text.style.display === "none") {
         text.style.display = "block";
         img.style.display = "none";
@@ -25,14 +26,3 @@ function abrirEnlace() {
     // Cambia la ubicación actual
     window.location.href = rutaArchivo;
 }
-
-function adjustZoom() {
-    if (window.innerWidth < window.innerHeight) {
-        document.body.style.zoom = "50%";
-    } else {
-        document.body.style.zoom = "100%";
-    }
-}
-
-window.onload = adjustZoom;
-window.addEventListener("orientationchange", adjustZoom);
